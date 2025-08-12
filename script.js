25bb310606ab7940a987d0e67ab85061359351b6
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-08-12 10:21:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function back() {
+﻿function back() {
     window.location.href = "https://miroshkin0207.github.io/miroshkin"
 }
 
 function buttonClick() {
     let total = document.querySelector("#total").innerHTML++
+}
+
+const names = ["Серафим", "Итачи", "Обито"]
+const scores = [100, 69, 52]
+
+for (let i = 0; i < names.length; i++) {
+    let line = document.createElement("div")
+    line.classList.add("line")
+
+    let name = document.createElement("div")
+    name.classList.add("names")
+    name.innerHTML = i + 1 + ". " + names[i]  
+    line.appendChild(name)
+
+    let score = document.createElement("div")
+    score.classList.add("scores")
+    score.innerHTML = scores[i]
+    line.appendChild(score)
+    
+    document.querySelector(".rect").appendChild(line)
 }
