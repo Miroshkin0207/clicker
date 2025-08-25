638e2a0646b8dda14ecc376862cb349840c691ef
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-08-25 19:57:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,23 +6,28 @@ function buttonClick() {
     let total = document.querySelector("#total").innerHTML++
 }
 
-const names = []
-const scores = []
+const table = [
+    { name: "Итачи", score: 1000 },
+    { name: "Обито", score: 485 },
+    { name: "Кагуя", score: 1581 },
+    { name: "Мадара", score: 1500 },
+    { name: "Хамура", score: 898 }
+]
+table.sort((a, b) => b.score - a.score)
 
-for (let i = 0; i < names.length; i++) {
+for (let i = 0; i < table.length; i++) {
     let line = document.createElement("div")
     line.classList.add("line")
 
     let name = document.createElement("div")
     name.classList.add("names")
-    name.innerHTML = i + 1 + ". " + names[i]  
+    name.innerHTML = i + 1 + ". " + table[i].name
     line.appendChild(name)
 
     let score = document.createElement("div")
     score.classList.add("scores")
-    score.innerHTML = scores[i]
+    score.innerHTML = table[i].score
     line.appendChild(score)
     
     document.querySelector(".rect").appendChild(line)
-
 }
